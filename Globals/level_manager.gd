25d0de0e695d1268e4def0f6e11cb66a18e0d1c4
TakaVII <ulirub@gmail.com>
2025-12-02 21:01:52 -0400

    Add multi-tilemap bounds management to level manager
    Introduces registration and combined bounds calculation for multiple TileMapLayer instances in level_manager.gd. This enables dynamic recalculation and emission of updated tilemap bounds when tilemaps are registered, improving support for multi-layered levels. Minor syntax fixes applied to array type hints.

--- Globals/level_manager.gd
+++ Globals/level_manager.gd
@@ -1,9 +1,37 @@
 extends Node
 
-signal tilemap_bounds_changed (bounds : Array [Vector2])
+signal tilemap_bounds_changed (bounds : Array[Vector2])
 
-var current_tilemap_bounds : Array [ Vector2 ]
+var current_tilemap_bounds : Array[Vector2]
+var _registered_tilemaps: Array[TileMapLayer] = []
+var _recalculation_scheduled: bool = false
 
-func change_tilemap_bounds (bounds : Array [Vector2]) -> void:
-	current_tilemap_bounds = bounds
-	tilemap_bounds_changed.emit ( bounds )
+func register_tilemap(tilemap: TileMapLayer) -> void:
+	if not tilemap in _registered_tilemaps:
+		_registered_tilemaps.append(tilemap)
+	
+	if not _recalculation_scheduled:
+		_recalculation_scheduled = true
+		call_deferred("_recalculate_combined_bounds")
+
+func _recalculate_combined_bounds() -> void:
+	if _registered_tilemaps.is_empty():
+		return
+
+	var combined_rect := _registered_tilemaps[0].get_used_rect()
+	if _registered_tilemaps.size() > 1:
+		for i in range(1, _registered_tilemaps.size()):
+			combined_rect = combined_rect.merge(_registered_tilemaps[i].get_used_rect())
+	
+	var tile_size: Vector2i = _registered_tilemaps[0].tile_set.tile_size
+	
+	var final_bounds: Array[Vector2] = [
+		Vector2(combined_rect.position * tile_size), # Top-left corner
+		Vector2(combined_rect.end * tile_size)        # Bottom-right corner
+	]
+	
+	current_tilemap_bounds = final_bounds
+	
+	tilemap_bounds_changed.emit(final_bounds)
+	
+	_recalculation_scheduled = false
--- Scripts/GeneralNodes/level_tilemap.gd
+++ Scripts/GeneralNodes/level_tilemap.gd
@@ -1,19 +1,7 @@
 class_name LevelTilemap extends TileMapLayer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	LevelManager.change_tilemap_bounds( get_tilemap_bounds() )
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func get_tilemap_bounds() -> Array[Vector2]:
-	var bounds : Array [Vector2] = []
-	bounds.append(
-		Vector2( get_used_rect().position * rendering_quadrant_size )
-	)
-	bounds.append(
-		Vector2( get_used_cells().end * rendering_quadrant_size )
-	)
-	return bounds
-	
+	# Instead of calculating bounds itself, it just registers with the LevelManager.
+	# The manager will then handle the logic of calculating the combined bounds.
+	LevelManager.register_tilemap(self)
